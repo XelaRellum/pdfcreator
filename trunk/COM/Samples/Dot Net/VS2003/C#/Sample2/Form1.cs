76c6f51811e35416798d2fd70af9784b75e1d257
thesmilyface <thesmilyface@9afcca14-e0e2-0310-87f0-a5661a4e1f95>
2006-09-30 08:29:08 +0000

    Improve the .Net com samples

--- trunk/COM/Samples/Dot Net/VS2003/C#/Sample2/Form1.cs
+++ trunk/COM/Samples/Dot Net/VS2003/C#/Sample2/Form1.cs
@@ -215,6 +215,9 @@ namespace Sample2
 			System.Runtime.InteropServices.Marshal.ReleaseComObject(pErr);
 			pErr = null;
 			GC.Collect();
+			GC.WaitForPendingFinalizers();
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
 		}
 	}
 }
